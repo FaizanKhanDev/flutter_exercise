d7bd0688ad5f53ea9f82bc26d0b0b40551ecaaeb
FaizanKhanDev <faizan480khan@gmail.com>
2024-02-11 22:49:10 -0800

    ADDED list Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,31 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Column(
-      textDirection: TextDirection.ltr,
-      children: [
-        // Expanded(
-        //     child: Text(
-        //   'One',
-        //   textDirection: TextDirection.ltr,
-        //   style: TextStyle(fontSize: 50),
-        // )),
-        Text(
-          'Two',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 50),
-        ),
-        Text(
-          'Three',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 50),
-        ),
-        Text(
-          'Four',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 50),
-        )
-      ],
-    ),
-  );
+  runApp(Directionality(
+    textDirection: TextDirection.ltr,
+    child: ListView(children: [
+      Container(
+        height: 300,
+        color: Colors.purpleAccent,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+        child: Center(
+            child: Text(
+          'First',
+          style: TextStyle(fontSize: 30),
+        )),
+      ),
+      Container(
+        height: 300,
+        color: Colors.blueAccent,
+        child: Center(child: Text('Second', style: TextStyle(fontSize: 30))),
+      ),
+      Container(
+        height: 500,
+        color: Colors.greenAccent,
+        child: Center(child: Text('Third', style: TextStyle(fontSize: 30))),
+      )
+    ]),
+  ));
 }
